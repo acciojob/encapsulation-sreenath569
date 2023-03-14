c77027decf9c54e1330818f67e705ed5bcc8e8d0
sreenath <sreenathkommuru@gmail.com>
2023-03-14 20:18:19 +0530

    code done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+
+        RWOnly obj = new RWOnly();
+        //java: name has private access in com.driver.RWOnly
+        obj.setName("sreenath");
+
+        System.out.print(obj.getName());
+    }
 }
